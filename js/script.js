44c6b97ef75ae7b62074b8be769fe269937d954d
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-04-19 01:57:04 -0500

    Testing

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-var map;
-const iit = {lat:41.835, lng:-87.627};
-const pier = {lat:41.8919, lng:-87.6051};
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 15, center:iit,
-	});
-  
-	const marker = new google.maps.Marker({
-		position: iit, map:map,
-	});
-}
-
-function worldView() {
-  map.setZoom(1);
-}
-
-function viewIIT(){
-  map.setCenter(iit)
-  map.setZoom(15);
-}
-
-function viewPier(){
-  map.setCenter(pier)
-  map.setZoom(13)
-}
-
 let slideIndex = 0;
 showSlides();
 
