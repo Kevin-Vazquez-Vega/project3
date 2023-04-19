073229392532f14d4aed8d2a116fc8b38e49d3a1
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-04-19 00:45:39 -0500

    Attempted to fix map

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 var map;
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById("map"), { 
-	center: {lat:41.835, lmg:-87.627},
-		zoom: 15, center: iit,
-	});
+  map = new google.maps.Map(document.getElementById("map"), { 
+    zoom: 15, center: iit,
+  });
+  const marker = new google.maps.Marker({
+    position: iit. map: map
 	
 const iit = {lat:41.835, lng:-87.627}
 const pier = {lat:41.8919, lng:-87.6051};
 
 function worldView() {
-	map.setZoom(1);
+  map.setZoom(1);
 }
 function viewIIT(){
-	map.setCenter(iit)
-	map.setZoom(15);
+  map.setCenter(iit)
+  map.setZoom(15);
 }
 function viewPier(){
-	map.setCenter(pier)
-	map.setZoom(13)
+  map.setCenter(pier)
+  map.setZoom(13)
 }
 
   let slideIndex = 1;
