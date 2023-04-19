b557f97d81e781485d229872e86aa37497c2aa37
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-04-18 23:36:53 -0500

    Attempted to fix maps and hobbies

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-var map;
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(41.87552, -87.630717),
+    zoom: 10,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  myPointer(map);
 
-const iit = {lat:41.835, lng:-87.627};
-const pier = {lat: 41.8919, lng: -87.6051};
-
-function initMap() {
-	map = new google.maps.Map(document.getElementByID("map"), {
-		zoom: 15, center: iit, });
-	
-	const marker = mew google.maps.Marker.({
-		position: iit, map:map.
-	});
+  google.maps.event.addListener(map, "click", function(event){
+    movePtr(map, event.latLng);
+  });
 }
 
 function worldView() {
