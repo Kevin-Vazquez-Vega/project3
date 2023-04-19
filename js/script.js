01bdcdc505515b7e57b1d78365aeacc2fadc1066
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-04-19 02:37:16 -0500

    Fixed slideshow and map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+var map;
+
+const iit = {lat:41.835, lng:-87.627};
+const pier = {lat: 41.892654, lng: -87.610168}
+
+function initmap(){
+	map = new google.maps.Map(document.getElementByID("map"), {
+		zoom: 15, center: iit,
+	});
+	const marker = new google.maps.Marker({
+		position: iit, map:map,
+	});
+}
+
+function worldView() {
+	map.setZoom(1);
+}
+
+function viewIIT() {
+	map.setCenter(iit);
+	map.setZoom(15);
+}
+
+function viewPier(){
+	map.setCenter(pier);
+	map.setZoom(15);
+}
+
 let slideIndex = 0;
 showSlides();
 
