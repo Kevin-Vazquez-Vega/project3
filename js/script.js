cf7a3b2aff5b1ada9567930022009b7d8de56465
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-04-19 16:05:25 -0500

    Changed map and fixed js on hobbies

--- js/script.js
+++ js/script.js
@@ -1,51 +1,62 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41, lng: -90},
+        center: {lat:41.835, lng:-87.627},
         zoom: 8
     });
 
-    const contentString =
+      const contentString =
     '<div id="content">' +
     '<h1 id="firstHeading" class="firstHeading">Chicago</h1>' +
     '<div id="bodyContent">' +
-    "<p>The city of Chicago</p>" + 
+    "<p>The city of Chicago.</p>" + 
     "</div>" +
     "</div>";
-	
     const infowindow = new google.maps.InfoWindow({
         content: contentString,
         ariaLabel: "Chicago",
     });
-    let marker = new google.maps.Marker({
-        position: { lat: 41.8781, lng: -87.6298 },
+     let marker = new google.maps.Marker({
+        position: { lat:41.835, lng:-87.627 },
         map,
-        title: "Chicago"
+        title: "The Illinois Institute of Technology"
+    })
+    marker.addListener("click", () => {
+        infowindow.open({
+        anchor: marker,
+        map,
+        });
     });
+    new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 10,
+      fillColor: "#530000",
+      fillOpacity: 0.5,
+      map,
+      center: {lat:41.835, lng:-87.627},
+      radius: 10000,
+    });
+}
 function worldView() {
 	map.setZoom(1);
 }
-
 function viewIIT() {
 	map.setCenter(iit);
 	map.setZoom(15);
 }
-
 function viewPier(){
 	map.setCenter(pier);
 	map.setZoom(15);
 }
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
